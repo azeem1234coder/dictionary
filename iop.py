fdc87063e432f968b5b319a9f75d6f181cefb7c8
azeem1234coder <odusanyaabdulazeem25@gmail.com>
2025-02-09 21:06:52 +0100

    890

---
+++ iop.py
@@ -0,0 +1,5 @@
+country_code = {'India':'0091','Australia' :'0025','nepal':'0077'}
+print("country code for India -")
+print(country_code.get('India',"not found"))
+print("country code for japan -")
+print(country_code.get('Japan',"not found"))
